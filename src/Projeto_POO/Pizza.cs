01cc3414e6ef49cce6a2217114f9639cdfcb2ee0
João Caram <caram@pucminas.br>
2026-08-20 21:04:18 -0300

    Pizza (stub), versão 1

---
+++ src/Projeto_POO/Pizza.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+
+namespace XulambsFoods {    
+    public class Pizza {
+
+        /// <summary>
+        /// Lembre-se:
+        // ENTENDER O PROBLEMA!!!
+        //Regra 0 -- não entre em pânico
+        //Regra 1 -- não viaje
+        /// </summary>
+        /// 
+        #region atributos
+        int _maxIngredientes;
+        double _precoBase;
+        int _quantIngredientes;
+        double _valorPorAdicional;
+        string _descricao;
+        #endregion
+
+        #region construtores
+        public Pizza() {
+            _descricao = "Pizza";
+            _maxIngredientes = 8;
+            _precoBase = 29d;
+            _quantIngredientes = 0;
+            _valorPorAdicional = 5d;
+        }
+
+        public Pizza(int adicionais) {
+        
+        }
+        #endregion
+
+        #region métodos privados
+        private double ValorAdicionais() {
+                
+        }
+
+        private void ModificarDescricao() {
+            _descricao = $"Pizza com {_quantIngredientes} adicionais";
+        }
+
+        private bool PodeAdicionar(int quantos) {
+                
+        }
+        #endregion
+
+        #region métodos públicos
+        public double CalcularValorFinal() {
+            return _precoBase + ValorAdicionais();
+        }
+
+        public int AdicionarIngredientes(int quantos) {
+            if (PodeAdicionar(quantos)) {
+                _quantIngredientes = _quantIngredientes + quantos;
+                ModificarDescricao();
+            }
+            return _quantIngredientes;
+        }
+
+        public string GerarCupom() {
+                
+        }
+        #endregion
+
+    }
+}
